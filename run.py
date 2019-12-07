c17e468d3410f9f2f34fd381c08bedbf3f19d27e
136ytr <44334104+136ytr@users.noreply.github.com>
2019-12-07 21:31:46 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from pytube import YouTube
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,10 @@ def index():
     return 'Hello!'
 
 if __name__ == '__main__':
+    yt = YouTube("http://www.youtube.com/watch?v=Ik-RsDGPI5Y")
+    yt.set_filename('myFirstVideo')
+    #video = yt.get('mp4','720p')
+    video = yt.filter('.mp4')[-1]
+    video.download('/')
+    
     app.run(host='0.0.0.0', debug=True)
