bf498dcd3a5b303c6194ce0df2f89218736b74e2
136ytr <44334104+136ytr@users.noreply.github.com>
2019-12-15 01:11:05 +0800

    Update run.py

--- run.py
+++ run.py
@@ -11,11 +11,14 @@ def index():
 def video():
     return render_template("video.html")
 
-if __name__ == '__main__':
+@app.route('/yt')
+def video():
     yt = YouTube("http://www.youtube.com/watch?v=Ik-RsDGPI5Y")
     yt.set_filename('myFirstVideo')
     #video = yt.get('mp4','720p')
     video = yt.filter('.mp4')[-1]
     video.download('/templates')
-    
+    return render_template("yt.html")
+
+if __name__ == '__main__':   
     app.run(host='0.0.0.0', debug=True)
