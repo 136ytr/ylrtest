d92f2cd70bfd8844b2406c66a866c4bc71dbf54c
136ytr <44334104+136ytr@users.noreply.github.com>
2019-12-15 03:43:36 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template
 from pytube import YouTube
-import os
 
 app = Flask(__name__,static_folder='static')
 
@@ -14,14 +13,9 @@ def video():
 
 @app.route('/yt')
 def video_yt():
-    #if not os.path.exists('/static'):
-    #    os.mkdir('/static')
     yt = YouTube("http://www.youtube.com/watch?v=Ik-RsDGPI5Y")
-    #yt.set_filename('myFirstVideo')
-    #video = yt.get('mp4','720p')
-    video = yt.streams.filter(file_extension='mp4').first()
+    video = yt.streams.filter(file_extension='mp4',res='480p').first()
     video.download(output_path='/app/static',filename='myFirstVideo')
-    #os.rename('/templates/'+yt.title+'.mp4','/templates/myFirstVideo.mp4')
     return render_template("yt.html")
 
 if __name__ == '__main__':   
