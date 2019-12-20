26cd232e82af3feced5b4baa0a85d55ceeadbec4
136ytr <44334104+136ytr@users.noreply.github.com>
2019-12-20 17:06:42 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from pytube import YouTube
 app = Flask(__name__,static_folder='static')
 
 @app.route('/')
-def index():
+def home():
     return 'Hello!'
 
 @app.route('/video')
@@ -17,6 +17,5 @@ def video_yt():
     video = yt.streams.filter(file_extension='mp4').first()
     video.download(output_path='/app/static',filename='myFirstVideo')
     return render_template("yt.html")
-
 if __name__ == '__main__':   
     app.run(host='0.0.0.0', debug=True)
